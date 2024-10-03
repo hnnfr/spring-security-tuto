712ee9a0c6bea9e56bfe9c40596c0887b3f9eeb1
hnnfr <59726038+hnnfr@users.noreply.github.com>
2024-10-03 09:49:43 +0200

    Set up the REST API as resource server so that I can use it with Keycloak, which acts as the auth server. That means the REST API will not handle the authentication anymore, but it will delegate that to Keycloak, and it will read the access token to get information about authentication/authorization.

--- src/main/java/fr/algofi/hnn/springsecuritytuto/config/AppSecurityConfig.java
+++ src/main/java/fr/algofi/hnn/springsecuritytuto/config/AppSecurityConfig.java
@@ -1,23 +1,17 @@
 package fr.algofi.hnn.springsecuritytuto.config;
 
-import fr.algofi.hnn.springsecuritytuto.filter.JWTGeneratorFilter;
-import fr.algofi.hnn.springsecuritytuto.filter.JWTValidatorFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
-import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableMethodSecurity
@@ -30,34 +24,25 @@ public class AppSecurityConfig {
 
     @Bean
     SecurityFilterChain customSecurityFilterChain(HttpSecurity http) throws Exception {
-        // accepting only HTTPS
-//        http.requiresChannel(rm -> rm.anyRequest().requiresSecure());
-//        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidsession").maximumSessions(1).maxSessionsPreventsLogin(true));
-
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
         http.csrf(AbstractHttpConfigurer::disable)
-//                .sessionManagement(smc -> smc.sessionCreationPolicy(SessionCreationPolicy.STATELESS))     // using this make test by browser fail
-                .addFilterBefore(new JWTValidatorFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new JWTGeneratorFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests) ->
                         requests.requestMatchers(HttpMethod.POST, "/users").access(
-                                    new WebExpressionAuthorizationManager("hasAuthority('WRITE_USER') && hasRole('ADMIN')"))
-                                .requestMatchers(HttpMethod.GET, "/users", "/users/{userId}").hasAnyAuthority("READ_USER", "WRITE_USER")
+                                        new WebExpressionAuthorizationManager("hasAuthority('WRITE_USER') && hasRole('ADMIN')"))
+//                                .requestMatchers(HttpMethod.GET, "/users", "/users/{userId}").hasAnyAuthority("READ_USER", "WRITE_USER")
+                                .requestMatchers(HttpMethod.GET, "/users", "/users/{userId}").hasRole("USER")
                                 .requestMatchers(HttpMethod.POST, "/topics/{topicId}/opinions").hasAuthority("WRITE_OPINION")
                                 .requestMatchers(HttpMethod.POST, "/topics").hasAuthority("WRITE_TOPIC")
                                 .requestMatchers(HttpMethod.GET, "/topics", "/topics/{topicId}").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/error").permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomeBasicAuthenticationEntryPoint()));
+        http.oauth2ResourceServer(rsc -> rsc.jwt(jwtConfigurer ->
+                jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
         return http.build();
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    CompromisedPasswordChecker compromisedPasswordChecker() {
-        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
